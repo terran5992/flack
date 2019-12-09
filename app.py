cf187ca2fa9bca9c1f64fc99308d5f70d78c4eff
terran5992 <56596358+terran5992@users.noreply.github.com>
2019-12-09 08:58:51 +0800

    First Commit

---
+++ app.py
@@ -0,0 +1,169 @@
+import os
+
+from flask import Flask, session, render_template, redirect, request, url_for,flash ,abort, jsonify
+from flask_session import Session
+from flask_socketio import SocketIO, emit, join_room, leave_room, send
+from datetime import datetime
+
+
+import requests
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['DEBUG'] = True
+socketio = SocketIO(app, manage_session=False)
+
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+users = {}
+rooms = {
+    'Green': {'messages':[] }
+    }
+ 
+
+class Message:
+
+    def __init__(self, message, username):
+        self.message = message
+        self.username = username
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S %d-%b-%y")
+        self.timestamp = current_time
+
+    def __repr__(self):
+        return "Msg: " + self.message + " By: " + self.username
+
+    
+
+
+@app.route('/')
+def index():
+    if session.get('username') == None :
+        return redirect(url_for('login'))
+    return render_template('index.html', rooms=rooms)
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        #check if form input is greater than 1 char
+        users[request.form.get('username')] = 'Green'
+        session['username'] = request.form.get('username')
+
+        
+
+
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    """ Log user out """
+    # Forget any user ID
+    session.clear()
+    flash('You have successfully logged out')
+
+    # Redirect user to index page
+    return redirect("/")
+
+
+#What does this line of code do?
+@socketio.on('message') # listening for events with the tag of (message)
+def handlemessage(data):
+    send(data, broadcast=True) # broacasts it to everyone with connected socekt
+
+@socketio.on('new_room')
+def receive_room_name(payload):
+    name = payload['room_name']
+    if name not in rooms:
+        rooms[name] = {"messages" : [] }
+        emit('receive_new_room', {'name': name})
+    else:
+        emit('receive_new_room', {'msg': 'Room Already Exists'})
+        pass #emit and error message
+    
+
+
+
+@socketio.on('username', namespace='/private')
+def receive_username(payload):
+    #users.append({username : request.sid})
+    if payload['oldusername'] in users:
+        del users[payload['oldusername']]
+
+    if payload['newusername'] in users:
+        print('Duplicate, return error')
+
+    users[payload['newusername']] = request.sid
+    print('User Added')
+    session['username'] = payload['newusername']
+    emit('receive_username', {'username': payload['newusername']})
+
+
+
+
+
+@socketio.on('private_message', namespace='/private')
+def private_message(payload): # received a dict object from event
+    recipient_session_id = users[payload['username']] 
+    message = payload['message']
+
+    print(users)
+    print(recipient_session_id)
+    print(message)
+
+    emit('new_private_message', message, room=recipient_session_id)
+
+
+@socketio.on('incoming-msg')
+def on_message(data):
+    """Broadcast messages"""
+
+    msg = data["msg"]
+    username = data["username"]
+    room = data["room"]
+    #Creating a message object and adding it to the list
+    obj = Message(msg,username)
+    if len(rooms[room]['messages']) > 100:
+        del(rooms[room]['messages'][0])
+    rooms[room]['messages'].append(obj)
+
+    
+    send({"username": obj.username , "msg": obj.message, "timestamp": obj.timestamp}, room=room)
+
+
+
+
+
+
+
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    # gets the username and change the room that it is in
+    users[session.get('username')]=room
+    #Gets history and formats it 
+    lst = []
+    for msg in rooms[room]['messages']:
+        lst.append({"msg":msg.message, "username": msg.username, "timestamp": msg.timestamp})
+    emit('get_history', {'history': lst})
+    send({'msg' :username + ' has entered the '+ room +' chat'}, room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send({'msg':username + ' has left the chat'}, room=room)
+
+if __name__ == '__main__':
+    socketio.run(app)
+
+# Do not use flask run as it wont have socketio wrapped
---
+++ static/scripts/main.js
@@ -0,0 +1,212 @@
+
+
+
+$(document).ready(function(){ // When document is loaded and ready, run this function
+
+
+    // connect to the socket, can use either a dynamic or harcoded url
+    var socket = io.connect('http://' + document.domain + ':' + location.port);
+
+    //declaring global 'room' variable
+    let room = "Green";
+    joinRoom("Green");
+
+
+    // When user connects to the web socket ^^, send the event to server
+    /* socket.on('connect', function(){
+        socket.send({'msg':'User has Connected!'});
+    }); */
+
+    // When a broadcasted message is received, append to the list
+    socket.on('message', function(data){
+
+        if (data.username){
+            $('#display_message').append(
+                "<div class='speech-wrapper'>" +
+                    "<div class='bubble'>"+
+                        "<div class='txt'>"+
+                        // Message parameters go here
+                        "<p class='name'>"+data.username+"</p>"+
+                        "<p class='message'>"+data.msg+"</p>"+
+                        "<span class='timestamp'>"+data.timestamp+"</span>"+
+                        "</div>"+
+                        "<div class='bubble-arrow'>"+"</div>"+
+                    "</div>"+
+                "</div>"
+            );
+        }
+        else{
+            printSysMsg(data.msg)
+        }
+        scrollDownChatWindow();
+    });
+
+    socket.on('get_history', function(data){
+        console.log('Recevied History')
+        // For each item in data
+        // append data.history to #display_message
+        let hist = data.history;
+        for( let i = 0, len=hist.length; i<len; i++){
+
+            $('#display_message').append(
+                "<div class='speech-wrapper'>" +
+                    "<div class='bubble'>"+
+                        "<div class='txt'>"+
+                        // Message parameters go here
+                        "<p class='name'>"+hist[i].username+"</p>"+
+                        "<p class='message'>"+hist[i].msg+"</p>"+
+                        "<span class='timestamp'>"+hist[i].timestamp+"</span>"+
+                        "</div>"+
+                        "<div class='bubble-arrow'>"+"</div>"+
+                    "</div>"+
+                "</div>"
+            );}
+        scrollDownChatWindow();
+
+        
+    });
+
+    function changeRoom(){
+            let newroom = li.innerHTML;
+            if (newroom == room){
+                msg = `You are already in ${room} room.`
+                printSysMsg(msg);
+            } else {
+                leaveRoom(room);
+                joinRoom(newroom);
+                room = newroom;
+            };
+    };
+
+
+    socket.on('receive_new_room', function(name){ // when room name is sent from the server
+        if (name.name){
+        const li = document.createElement('a');
+        li.setAttribute('class','select-room list-group-item list-group-item-action bg-light');
+        li.setAttribute('href','#')
+        li.innerHTML = name.name;
+        li.addEventListener('click', function(){
+            let newroom = li.innerHTML;
+            if (newroom == room){
+                msg = `You are already in ${room} room.`
+                printSysMsg(msg);
+            } else{
+                leaveRoom(room);
+                joinRoom(newroom);
+                room = newroom;
+            };
+
+        });
+        $('#room_list').append(li); // append it to the list of rooms
+        leaveRoom(room);
+        joinRoom(name.name);
+        room = name.name;
+        }
+        else {
+            printSysMsg(name.msg)
+        };
+
+    });
+
+    $('#send_button').on('click', function() { // what happens when button is clicked
+        message = $('#user_message').val();
+        socket.emit('incoming-msg',{'msg':message, 'room': room, 'username': username}); // sends the message to server // how does that link w top func??
+        $('#user_message').val(''); // emptys out the input field after submit
+    });
+
+    $('#create_room_btn').on('click', function() { // what happens when button is clicked
+        var newroomname = $('#inlineFormInputGroup').val();
+        socket.emit('new_room', {'room_name': newroomname}); // sends the message to server // how does that link w top func??
+        $('#inlineFormInputGroup').val(''); // emptys out the input field after submit
+    });
+
+    // Creates a private socket to connect to 
+    var private_socket = io('http://' + document.domain + ':' + location.port + '/private'); 
+
+    // What happens when you click the button to change username
+    $('#send_username').on('click',function(){
+        var newusername = $('#username').val();
+        var oldusername = document.getElementById("change_username_btn").innerHTML;
+        private_socket.emit('username',{'newusername' : newusername, 'oldusername': oldusername});
+        $('#username').val('');
+    });
+
+    // When the client receives "receive_username" event
+    private_socket.on('receive_username', function(data){
+        var newusername = data.username;
+        $("#logout_btn").attr("hidden", false);
+
+        document.getElementById("change_username_btn").innerHTML = newusername;
+
+    });
+
+    // When the client clicks to send a private message
+    $('#send_private_message').on('click', function(){
+        var recipient = $('#send_to_username').val();
+        var message_to_send = $('#private_message').val();
+        var sender = document.getElementById("username-navbar").innerHTML;
+        private_socket.emit('private_message', {'username': recipient , 'message' : message_to_send, 'sender': sender});
+
+    });
+
+    private_socket.on('new_private_message', function(msg){
+        alert(msg);
+    });
+
+
+    document.querySelectorAll('.select-room').forEach(a => {
+        a.addEventListener('click', () => {
+            let newroom = a.innerHTML;
+            if (newroom == room){
+                msg = `You are already in ${room} room.`
+                printSysMsg(msg);
+            } else {
+                leaveRoom(room);
+                joinRoom(newroom);
+                room = newroom;
+            }
+        })
+    });
+
+
+    // Leave Room Function
+    function leaveRoom(room){
+        socket.emit('leave', {'username': username, 'room': room});
+    };
+
+     // Join Room Function
+    function joinRoom(room){
+        socket.emit('join', {'username': username, 'room': room});
+
+        // Clears Message Area
+        document.querySelector('#display_message').innerHTML = '';
+        // Prints a user has joined the page
+
+        // Autofocus on text box
+        document.querySelector("#user_message").focus();        
+        
+    };
+
+    // Prints System Messages
+    function printSysMsg(msg){
+        const p = document.createElement('p');
+        p.innerHTML = msg;
+        document.querySelector('#display_message').append(p);
+    }
+
+    // Make 'enter' key submit message
+    let msg = document.getElementById("user_message");
+    msg.addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("send_button").click();
+        }
+    });
+
+    // Scroll chat window down
+    function scrollDownChatWindow() {
+        const chatWindow = document.querySelector("#display_message");
+        chatWindow.scrollTop = chatWindow.scrollHeight;
+    };
+
+});
